5e4809e77ebc8b548217217ad4e0b22dc1c4ec01
Aaron Oppenheimer <aoppenheimer@gmail.com>
2016-09-03 12:32:53 -0400

    fetching email

--- spud.py
+++ spud.py
@@ -2,10 +2,16 @@
 import spudemail
 
 def main():
-    spudemail.sendMail(
-        "aoppenheimer@gmail.com",
-        "test",
-        "this is a test email!")
+
+#     spudemail.sendMail(
+#         "aoppenheimer@gmail.com",
+#         "test",
+#         "this is a test email!")
+    recd = spudemail.getMail()
+    
+    for r in recd:
+        print('from: {0}   subj: {1}'.format(r['from'], r['subj']))
+        spudemail.sendMail(r['from'], 'regarding {0}'.format(r['subj']), 'thanks!')
 
 if __name__ == "__main__":
     main()
--- spudemail.py
+++ spudemail.py
@@ -1,6 +1,13 @@
+from __future__ import unicode_literals
+
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+import email
+
+from imapclient import IMAPClient
+
 
 def sendMail(recipient, subject, message):
     """this is some test documentation in the function"""
@@ -16,6 +23,15 @@ def sendMail(recipient, subject, message):
     msg['Subject'] = subject
     msg.attach(MIMEText(message))
 
+
+    fp = open('pic.jpg', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', 'pic')
+    msg.attach(msgImage)
+
     try:
         print('sending mail to ' + recipient + ' on ' + subject)
 
@@ -30,7 +46,37 @@ def sendMail(recipient, subject, message):
     except Exception as e:
         print(str(e))
  
-    
+
+def getMail():
+
+    SECRET = getSecret()
+
+
+    server = IMAPClient("imap.gmail.com", use_uid=True, ssl=True)
+    server.login("spudwalks@gmail.com", SECRET)
+
+    select_info = server.select_folder('INBOX')
+    print('%d messages in INBOX' % select_info['EXISTS'])
+
+    messages = server.search(['NOT', 'DELETED'])
+    print("%d messages that aren't deleted" % len(messages))
+
+    print("Messages:")
+    theMsgs = []
+    response = server.fetch(messages, ['RFC822'])
+    for messagegId,data in response.iteritems():
+            messageString = data['RFC822']
+            msgStringParsed = email.message_from_string(messageString)
+            messageFrom = msgStringParsed['From']
+            messageSubj = msgStringParsed['Subject']
+            theMsgs.append({'from':messageFrom, 'subj':messageSubj})
+            print 'From:{0}\nSubject:{1}'.format(messageFrom, messageSubj)
+                                                
+    print('keys: {0}'.format(response.keys()))
+
+    server.logout()
+    return theMsgs
+
 def getSecret():
     f = open('secret.txt')
     line = f.readline().strip()
