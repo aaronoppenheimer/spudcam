7b61dab9aac855ddecc551c81da9daa796974544
aaronoppenheimer <aoppenheimer@gmail.com>
2016-09-09 11:22:30 -0400

    added logging

--- runner.py
+++ runner.py
@@ -4,7 +4,8 @@ def main():
     while(True):
         subprocess.call(["git","pull"])
         subprocess.call(["crontab","-u","pi","/home/pi/spudcam/crontab.txt"])
-        subprocess.call(["python","/home/pi/spudcam/spud.py"])
+        with open('/home/pi/spudcam/logs/runnerlog.txt', 'w') as f:
+            subprocess.call(["python","/home/pi/spudcam/spud.py"], stdout=f, stderr=f)
 
 if __name__ == "__main__":
     main()
