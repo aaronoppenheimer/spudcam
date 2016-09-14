6b9cbff81e029ba52f83deadda5925db4b36b4b3
aaronoppenheimer <aoppenheimer@gmail.com>
2016-09-14 19:36:08 -0400

    made output unbuffered

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ def main():
         subprocess.call(["git","pull"])
         subprocess.call(["crontab","-u","pi","/home/pi/spudcam/crontab.txt"])
         with open('/home/pi/spudcam/logs/runnerlog.txt', 'a') as f:
-            subprocess.call(["python","/home/pi/spudcam/spud.py"], stdout=f, stderr=f)
+            subprocess.call(["python","-u","/home/pi/spudcam/spud.py"], stdout=f, stderr=f)
 
 if __name__ == "__main__":
     main()
