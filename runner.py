d3b2c603b5076db49ab7128edbd66d3500440aee
aaronoppenheimer <aoppenheimer@gmail.com>
2016-09-15 16:52:26 -0400

    bugfix

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ def main():
         subprocess.call(["git","pull"])
         subprocess.call(["crontab","-u","pi","/home/pi/spudcam/crontab.txt"])
         subprocess.call(["python","/home/pi/spudcam/spudlog.py"])
-        subprocess.call(["rm","/home/pi/spudcam/spudlog.py"])
+        subprocess.call(["rm","/home/pi/spudcam/logs/runnerlog.txt"])
         with open('/home/pi/spudcam/logs/runnerlog.txt', 'a') as f:
             subprocess.call(["python","-u","/home/pi/spudcam/spud.py"], stdout=f, stderr=f)
 
