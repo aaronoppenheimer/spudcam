38a8ae1d6df23f9666df7f46c7ffd2a289bd1ec6
aaronoppenheimer <aoppenheimer@gmail.com>
2016-10-09 18:22:46 -0400

    added temp sensing

--- spud.py
+++ spud.py
@@ -5,7 +5,7 @@ import sys
 import datetime
 from log import logit
 
-VERSION = 4
+VERSION = 5
 
 def main():
 
@@ -13,22 +13,30 @@ def main():
     logit('started at {0}'.format(datetime.datetime.now()))
 
     def restart(who):
-        logit('restarting on command')
+        logit('restarting on command from {0}'.format(who))
         return True # quit the program
     
     def heartbeat(who):
-        logit('heatbeating on command')
+        logit('heatbeating on command from {0}'.format(who))
         spudemail.sendMail(recipient=r['from'], subject='Heartbeat', message='Baboom!')
         return False
 
     def picture(who):
-        logit('sending picture on command')
+        logit('sending picture on command from {0}'.format(who))
         spudemail.sendMail(recipient=r['from'], subject='Picture', message='Baboom!', picture='/tmp/pic.jpg')
         return False
         
     def log(who):
-        logit('sending log on command')
+        logit('sending log on command from {0}'.format(who))
         spudemail.sendMail(recipient=r['from'], subject='Log File', file='/home/pi/spudcam/logs/runnerlog.txt')
+
+    def temp(who):
+        logit('sending temperature on command from {0}'.format(who))
+        f=open('/sys/class/thermal/thermal_zone0/temp')
+        line=f.readline()
+        f.close()
+        temperature=int(line)/1000
+        spudemail.sendMail(recipient=r['from'], subject='Spud Temp', message='Temperature:{0}'.format(temperature))
 
     while(True):
         try:
@@ -42,7 +50,8 @@ def main():
         doit = {'restart': restart,
                 'heartbeat': heartbeat,
                 'picture': picture,
-                'log': log
+                'log': log,
+                'temp': temp
                 }
 
         for r in recd:
--- spudemail.py
+++ spudemail.py
@@ -89,6 +89,7 @@ def getMail():
 def getSecret():
     f = open('secret.txt')
     line = f.readline().strip()
+    f.close()
     return line
 
 
