800a8446262c3c84fa202f221c7a0efc8af9413e
aaronoppenheimer <aoppenheimer@gmail.com>
2016-10-22 16:05:52 -0400

    fixed bug

--- camera.py
+++ camera.py
@@ -1,17 +1,20 @@
+from log import logit
 from picamera import PiCamera
 from time import sleep
-from DateTime import DateTime
+from datetime import datetime
 
 def takePicture(filename):
-    camera = PiCamera()
-    try:
-	    camera.rotation = 180
-	    sleep(4)
-	    n=DateTime.now()
-	    camera.exif_tags['IFD0.DateTime']=n.strftime("%Y:%m:%d %H:%M:%S")
-	    camera.capture(filename)
-    finally:
-        camera.close()
+	camera = PiCamera()
+	try:
+		camera.rotation = 180
+		sleep(4)
+		n=datetime.now()
+		camera.exif_tags['IFD0.DateTime']=n.strftime("%Y:%m:%d %H:%M:%S")
+		camera.capture(filename)
+	except Exception as e:
+		logit('camera problem: {0}'.format(str(e)))
+	finally:
+		camera.close()
 
 if __name__ == "__main__":
     takePicture("/tmp/pic.jpg")
