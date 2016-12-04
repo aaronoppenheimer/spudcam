21ea87e10154516892d1e4ea1d2c65005769b404
aaronoppenheimer <aoppenheimer@gmail.com>
2016-12-04 09:32:15 -0500

    foo

--- spud.py
+++ spud.py
@@ -20,7 +20,8 @@ def main():
     def reboot(who, subj=''):
         logit('hard reboot on command from {0}'.format(who))
         try:
-            call('/home/pi/spudcam/reboot.sh')
+            call('ls /home/pi/spudcam > /home/pi/spudcam/logs/runnerlog.txt')
+#            call('/home/pi/spudcam/reboot.sh')
         except:
             logit("Could not call reboot script.")
         return False
