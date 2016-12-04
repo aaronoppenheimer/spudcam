3adde69d3100e83910d02797bba5c1f287ad8b9f
aaronoppenheimer <aoppenheimer@gmail.com>
2016-12-04 09:44:46 -0500

    foo

--- spud.py
+++ spud.py
@@ -6,7 +6,7 @@ import datetime
 from log import logit
 from subprocess import call
 
-VERSION = 14.4
+VERSION = 14.5
 
 def main():
 
@@ -20,8 +20,7 @@ def main():
     def reboot(who, subj=''):
         logit('hard reboot on command from {0}'.format(who))
         try:
-            call(["ls", "-l", "/home/pi/spudcam"])
-#            call('/home/pi/spudcam/reboot.sh')
+            call(["/home/pi/spudcam/reboot.sh"])
         except:
             logit("Could not call reboot script.")
         return False
