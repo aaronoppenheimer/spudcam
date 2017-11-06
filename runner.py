d81593ed07e4232f624aa7dcc5618f2b4df174ef
Aaron Oppenheimer <aoppenheimer@gmail.com>
2017-11-06 14:49:52 +0000

    random

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 def main():
     while(True):
@@ -6,8 +7,11 @@ def main():
         subprocess.call(["crontab","-u","pi","/home/pi/spudcam/crontab.txt"])
         subprocess.call(["python","/home/pi/spudcam/spudlog.py"])
         subprocess.call(["rm","/home/pi/spudcam/logs/runnerlog.txt"])
+        time.sleep(15)
         with open('/home/pi/spudcam/logs/runnerlog.txt', 'a') as f:
             subprocess.call(["python","-u","/home/pi/spudcam/spud.py"], stdout=f, stderr=f)
 
 if __name__ == "__main__":
     main()
+
+
