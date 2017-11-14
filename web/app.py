464ea1fe0b8160f75e4f3f95914f531ddb9c375c
Aaron Oppenheimer <aoppenheimer@gmail.com>
2017-11-14 16:42:42 +0000

    web server for image

---
+++ web/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    print(path)
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
