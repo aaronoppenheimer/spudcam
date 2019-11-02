98f848783bbe6b2e73763fd16c6c4693b3d07501
aaronoppenheimer <aoppenheimer@gmail.com>
2019-11-02 12:08:35 -0400

    added movie

--- sunrise.sh
+++ sunrise.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 python /home/pi/src/spudcam/series.py
+ffmpeg -framerate 25 -i /home/pi/src/spudcam/pix/series%02d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -y /home/pi/src/spudcam/pix/sunrise.mp4
+
